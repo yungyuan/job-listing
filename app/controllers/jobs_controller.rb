11e721f907de0c0b5893fa94a8160a0b4aef2b68
Yung Yuan <leonychuang@gmail.com>
2016-07-26 01:07:19 +0800

    implement jobs#edit jobs#update & title cannot be blank & jobs#destroy

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -21,6 +21,26 @@ class JobsController < ApplicationController
     @jobs = Job.all
   end
 
+  def edit
+    @job = Job.find(params[:id])
+  end
+
+  def update
+    @job = Job.find(params[:id])
+    if @job.update(job_params)
+      redirect_to jobs_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @job = Job.find(params[:id])
+
+    @job.destroy
+    redirect_to jobs_path
+  end
+
   private
 
   def job_params
