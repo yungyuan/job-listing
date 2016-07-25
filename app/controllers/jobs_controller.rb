66ac0d6a8dfbd79dce2ed0af2ce996f59a861eb4
Yung Yuan <leonychuang@gmail.com>
2016-07-26 01:01:40 +0800

    implement jobs#new & jobs#create

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,9 +1,29 @@
 class JobsController < ApplicationController
+  def new
+    @job = Job.new
+  end
+
+  def create
+    @job = Job.new(job_params)
+
+    if @job.save
+      redirect_to jobs_path
+    else
+      render :new
+    end
+  end
+
   def show
     @job = Job.find(params[:id])
   end
 
   def index
     @jobs = Job.all
+  end
+
+  private
+
+  def job_params
+    params.require(:job).permit(:title, :description)
   end
 end
