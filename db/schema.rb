52aec9d3601a4d00758732fe908fae6e2c9eb108
Yung Yuan <leonychuang@gmail.com>
2016-07-25 23:03:58 +0800

    generate job model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160725035255) do
+ActiveRecord::Schema.define(version: 20160725150323) do
+
+  create_table "jobs", force: :cascade do |t|
+    t.text     "title"
+    t.text     "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
