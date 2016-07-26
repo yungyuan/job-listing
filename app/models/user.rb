d21b09ef443dfbf24b42ebf890c10c3bd193dc26
Yung Yuan <leonychuang@gmail.com>
2016-07-26 11:12:33 +0800

    admin? use DB to confirm

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   def admin?
-    email == 'leonychuang@gmail.com'
+    is_admin
   end
 end
