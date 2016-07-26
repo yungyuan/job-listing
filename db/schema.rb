98d653377692fea711ad3744d10fc47d8afd484f
Yung Yuan <leonychuang@gmail.com>
2016-07-26 12:02:36 +0800

    wage_lower&upper and email

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160726031927) do
+ActiveRecord::Schema.define(version: 20160726035309) do
 
   create_table "jobs", force: :cascade do |t|
     t.text     "title"
