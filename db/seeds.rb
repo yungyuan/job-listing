25a128e98fc32b815b276357372b44f6231af7f4
Yung Yuan <leonychuang@gmail.com>
2016-07-28 21:17:54 +0800

    seed.db to generate data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,13 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-u = User.new
-u.email = "leonychuang@gmail.com"           # 可以改成自己的 email
 
-u.password = "123456"              # 最少要八碼
+puts "這個種子檔會自動建立一個admin帳號, 並且創建 10 個 public jobs, 以及10個hidden jobs"
 
-u.password_confirmation = "123456" # 最少要八碼
+create_account = User.create([email: 'example@gmail.com', password: '12345678', password_confirmation: '12345678', is_admin: 'true'])
+puts "admin created"
 
-u.is_admin = true
-u.save
-
+create_jos = for i in 1..10 do
+  Job.create!([title: "Job no.#{i}", description: "這是用種子建立的第 #{i} 個Public工作", wage_upper_bound: rand(50..99)*100, wage_lower_bound: rand(10..49)*100, is_hidden: "false"])
+  Job.create!([title: "Job no.#{i}", description: "這是用種子建立的第 #{i} 個Hidden工作", wage_upper_bound: rand(50..99)*100, wage_lower_bound: rand(10..49)*100,is_hidden: "true"])
+end
