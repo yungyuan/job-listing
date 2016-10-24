474ee3d960aab7cf27729af6bb6b2d4450220e3b
Yung Yuan <leonychuang@gmail.com>
2016-10-24 21:59:10 +0800

    view resumes

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,10 +13,12 @@ puts "Admin account created."
 
 create_jos = for i in 1..10 do
   Job.create!([title: "Job no.#{i}", description: "這是用種子建立的第 #{i} 個Public工作", wage_upper_bound: rand(50..99)*100, wage_lower_bound: rand(10..49)*100, is_hidden: "false"])
+  #sleep rand(1..5)
 end
 puts "10 Public jobs created."
 
 create_jos = for i in 1..10 do
   Job.create!([title: "Job no.#{i+10}", description: "這是用種子建立的第 #{i+10} 個Hidden工作", wage_upper_bound: rand(50..99)*100, wage_lower_bound: rand(10..49)*100,is_hidden: "true"])
+  #sleep rand(2..4)
 end
 puts "10 Hidden jobs created."
